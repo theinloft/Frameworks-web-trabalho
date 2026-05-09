fb80b62c1464c99961599f6f7dddbdc0e5317bfd
Thiago R Einloft <theinloft@gmail.com>
2026-05-09 18:59:03 -0300

    Bugfix: removendo Serviços que não serão utilizados, ajustando informações da base de dados.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProdutoModule } from './produto/produto.module';
 import { Categoria } from './categoria/entities/categoria.entity';
 import { Produto } from './produto/entities/produto.entity';
@@ -15,8 +13,8 @@ import { CategoriaModule } from './categoria/categoria.module';
       host: 'localhost',
       port: 5432,
       username: 'postgres',
-      password: 'senacrs',
-      database: 'scaffolding_produtos',
+      password: '123456',
+      database: 'Ecommerce_FW',
       autoLoadEntities: true,
       synchronize: true,
       entities: [Produto, Categoria],
@@ -26,7 +24,5 @@ import { CategoriaModule } from './categoria/categoria.module';
 
     CategoriaModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}

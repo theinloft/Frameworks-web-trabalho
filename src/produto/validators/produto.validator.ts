a6fa65f64dad9445ebb1f7410192135c0fb10006
Thiago R Einloft <theinloft@gmail.com>
2026-05-10 17:53:48 -0300

    Feature: adicionando validator Produto.

---
+++ src/produto/validators/produto.validator.ts
@@ -0,0 +1,35 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateProdutoDto } from '../dto/create-produto.dto';
+import { Repository } from 'typeorm';
+import { Produto } from '../entities/produto.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class ProdutoValidator {
+  constructor(
+    @InjectRepository(Produto)
+    private readonly repositoryProduto: Repository<Produto>,
+  ) {}
+
+  async validateProduto(dto: CreateProdutoDto) {
+    const produtoExistente = await this.repositoryProduto.findOneBy({
+      nome: dto.nome,
+    });
+
+    if (produtoExistente) {
+      throw new BadRequestException('Já existe um produto com esse nome');
+    }
+
+    if (dto.nome && dto.nome.length < 3) {
+      throw new BadRequestException('Nome inválido');
+    }
+
+    if (dto.preco !== undefined && dto.preco <= 0) {
+      throw new BadRequestException('Preço inválido');
+    }
+
+    if (dto.categoriaId !== undefined && dto.categoriaId <= 0) {
+      throw new BadRequestException('Categoria inválida');
+    }
+  }
+}

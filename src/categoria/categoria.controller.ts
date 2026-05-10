fa2d7868985898fd028b829ff1895feca3ae01ac
Thiago R Einloft <theinloft@gmail.com>
2026-05-10 17:54:08 -0300

    Feature: Adicionando Documentação Swagger para Categoria.

--- src/categoria/categoria.controller.ts
+++ src/categoria/categoria.controller.ts
@@ -10,27 +10,72 @@ import {
 import { CategoriaService } from './categoria.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('categoria')
 export class CategoriaController {
   constructor(private readonly categoriaService: CategoriaService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar categoria',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Criado',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro de validação',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Não autorizado',
+  })
   async create(@Body() createCategoriaDto: CreateCategoriaDto) {
     return this.categoriaService.create(createCategoriaDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar categorias',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
   findAll() {
     return this.categoriaService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Obter categoria pelo ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Não encontrado',
+  })
   findOne(@Param('id') id: string) {
     return this.categoriaService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar categoria',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Não encontrado',
+  })
   update(
     @Param('id') id: string,
     @Body() updateCategoriaDto: UpdateCategoriaDto,
@@ -39,6 +84,17 @@ export class CategoriaController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Remover categoria',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Não encontrado',
+  })
   remove(@Param('id') id: number) {
     return this.categoriaService.remove(+id);
   }

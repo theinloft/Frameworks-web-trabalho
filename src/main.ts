641813a3b1d56fd2ba4b70e0e84e572ddba6979c
Thiago R Einloft <theinloft@gmail.com>
2026-05-12 23:38:56 -0300

    Feature: adicionando GlobalPipes para evitar campos indevidos nas requisições das APIs.

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle("Ecommerce")
-  .setDescription("API Ecommerce")
-  .setVersion('1.0')
-  .build();
+    .setTitle('Ecommerce')
+    .setDescription('API Ecommerce')
+    .setVersion('1.0')
+    .build();
 
-  const documentFactory = () => SwaggerModule.createDocument(app,config);
-  SwaggerModule.setup('api',app,documentFactory);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }

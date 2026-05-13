73631e468083356a9e19f61f6c6eb66524d9c1bd
Thiago R Einloft <theinloft@gmail.com>
2026-05-12 23:59:07 -0300

    Feature: adicionando documentação swagger para pedido controller.

--- src/pedido/pedido.controller.ts
+++ src/pedido/pedido.controller.ts
@@ -10,32 +10,85 @@ import {
 import { PedidoService } from './pedido.service';
 import { CreatePedidoDto } from './dto/create-pedido.dto';
 import { UpdatePedidoDto } from './dto/update-pedido.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Pedido')
 @Controller('pedido')
 export class PedidoController {
   constructor(private readonly pedidoService: PedidoService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar pedido',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'Pedido criado com sucesso',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro de validação',
+  })
   create(@Body() createPedidoDto: CreatePedidoDto) {
     return this.pedidoService.create(createPedidoDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar pedidos',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pedidos listados com sucesso',
+  })
   findAll() {
     return this.pedidoService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Obter pedido',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pedido obtido com sucesso',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Pedido não encontrado',
+  })
   findOne(@Param('id') id: string) {
     return this.pedidoService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Atualizar pedido',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pedido atualizado com sucesso',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Pedido não encontrado',
+  })
   update(@Param('id') id: string, @Body() updatePedidoDto: UpdatePedidoDto) {
     return this.pedidoService.update(id, updatePedidoDto);
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Remover pedido',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pedido removido com sucesso',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Pedido não encontrado',
+  })
   remove(@Param('id') id: string) {
     return this.pedidoService.remove(id);
   }

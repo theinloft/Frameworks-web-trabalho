329582e2ccb5ea93a4532abb3c06e9aa0733950b
Thiago R Einloft <theinloft@gmail.com>
2026-05-13 23:47:04 -0300

    Bugfix: ajustando documentação swagger de update pedido, ajustando validação de array, para evitar erros no update.

--- src/pedido/dto/update-pedido.dto.ts
+++ src/pedido/dto/update-pedido.dto.ts
@@ -1,4 +1,23 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreatePedidoDto } from './create-pedido.dto';
+import { Type } from 'class-transformer';
+import { IsArray, IsUUID, ValidateNested } from 'class-validator';
+import { CreatePedidoItemDto } from './create-pedido-item.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdatePedidoDto extends PartialType(CreatePedidoDto) {}
+export class UpdatePedidoDto {
+  @ApiProperty({
+    description: 'ID do cliente',
+    example: '550e8400-e29b-41d4-a716-446655440000',
+  })
+  @IsUUID()
+  clienteId!: string;
+
+  @ApiProperty({
+    description: 'Lista de itens do pedido',
+    type: CreatePedidoItemDto,
+    isArray: true,
+  })
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreatePedidoItemDto)
+  itens!: CreatePedidoItemDto[];
+}

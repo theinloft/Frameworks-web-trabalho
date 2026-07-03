c93c95d47fcc7cd57085768d153c02b6f7fc42bc
Thiago R Einloft <theinloft@gmail.com>
2026-07-02 23:34:40 -0300

    bugfix: protegendo as URLs para quando não tiver token, redirecionar para o login.

--- frontend/src/Pages/LayoutPainel.tsx
+++ frontend/src/Pages/LayoutPainel.tsx
@@ -1,7 +1,12 @@
-import { Outlet } from "react-router-dom";
-import NavbarPainel from "../components/NavBarPainel/NavBarPainel";
+import { Navigate, Outlet } from 'react-router-dom';
+import NavbarPainel from '../components/NavBarPainel/NavBarPainel';
 
 export default function LayoutPainel() {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
   return (
     <>
       <NavbarPainel />
